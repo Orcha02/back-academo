5dd756b8f0832f130b94da7d1cd9b0695de4314c
Orcha02 <orcha2002@gmail.com>
2022-10-04 14:46:43 -0500

    Add views

--- backAcademo/registration_and_login/views.py
+++ backAcademo/registration_and_login/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import  CreateUserForm
+
+def registerPage(request):
+		context = {}
+		return render(request, 'registration_and_login/register.html', context)
+
+def loginPage(request):
+		context = {}
+		return render(request, 'registration_and_login/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
