5a3983b35fbdd4b16c25adf281c8f2fb0aecabf6
Orcha02 <orcha2002@gmail.com>
2022-10-04 15:05:26 -0500

    First form for user creation working

--- backAcademo/registration_and_login/views.py
+++ backAcademo/registration_and_login/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from .forms import  CreateUserForm
 
 def registerPage(request):
-		context = {}
-		return render(request, 'registration_and_login/register.html', context)
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'registration_and_login/register.html', context)
 
 def loginPage(request):
-		context = {}
-		return render(request, 'registration_and_login/login.html', context)
+    context = {}
+    return render(request, 'registration_and_login/login.html', context)
 
 def logoutUser(request):
 	logout(request)
