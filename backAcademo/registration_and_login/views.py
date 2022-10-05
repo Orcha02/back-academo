0e33689d0ed75cdb63e38a883c006df148fa6497
Orcha02 <orcha2002@gmail.com>
2022-10-04 22:44:36 -0500

    Add functional login,register and logout with basic main page

--- backAcademo/registration_and_login/views.py
+++ backAcademo/registration_and_login/views.py
@@ -1,38 +1,54 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 # Create your views here.
-from .forms import  CreateUserForm
+from .forms import CreateUserForm
+
 
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get("username")
+                messages.success(request, "Account was created for " + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'registration_and_login/register.html', context)
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get("username")
-            messages.success(request, "Account was created for " + user)
-            return redirect('login')
-    context = {'form':form}
-    return render(request, 'registration_and_login/register.html', context)
 
 def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+			user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Credenciales de acceso inválidas')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-    context = {}
-    return render(request, 'registration_and_login/login.html', context)
+		context = {}
+		return render(request, 'registration_and_login/login.html', context)
 
 def logoutUser(request):
-	logout(request)
-	return redirect('login')
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def home(request):
+    context = {}
+    return render(request, 'registration_and_login/main.html', context)
