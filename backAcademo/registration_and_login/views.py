1d81ae7f6d5fe1356c06d85f5c04ac7b63beb76b
Orcha02 <orcha2002@gmail.com>
2022-10-04 19:56:06 -0500

    Handle form errors & implement a flash message when user created

--- backAcademo/registration_and_login/views.py
+++ backAcademo/registration_and_login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 from .forms import  CreateUserForm
@@ -12,6 +13,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + user)
             return redirect('login')
     context = {'form':form}
     return render(request, 'registration_and_login/register.html', context)
